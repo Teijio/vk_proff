f4a0999810d26e7611aea72dcc229a07596f21d5
Teijio <ridpfrep@yandex.ru>
2024-05-02 20:18:42 +0300

    init

--- app/main.py
+++ app/main.py
@@ -1,27 +1,17 @@
-import uvicorn
-
-
 from fastapi import FastAPI
-
-
-class Boo:
-    pass
-
-
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 # from app.api.httpx_client import on_shutdown, on_start_up
 # from app.api.routers import main_router
 # from app.core.config import settings
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# origins = ["*"]
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 # app.include_router(main_router)
